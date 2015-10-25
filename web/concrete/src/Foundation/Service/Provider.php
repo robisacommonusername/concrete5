d6d3328f98b2f96a19f96d7195d3e6a5e9388d80
Korvin Szanto <korvinszanto@gmail.com>
2015-10-25 14:54:28 -0700

    Make provider implement application aware and have a provides function

--- web/concrete/src/Foundation/Service/Provider.php
+++ web/concrete/src/Foundation/Service/Provider.php
@@ -2,14 +2,39 @@
 namespace Concrete\Core\Foundation\Service;
 use \Concrete\Core\Application\Application;
 use Closure;
+use Concrete\Core\Application\ApplicationAwareInterface;
+use Illuminate\Container\Container;
 
 /**
  *  Extending this class allows groups of services to be registered at once.
  */
-abstract class Provider {
+abstract class Provider implements ApplicationAwareInterface {
 
-	public function __construct(Application $app) {
-		$this->app = $app;
+	public $app;
+
+	public function __construct(Container $app)
+	{
+		$this->setApplication($app);
+	}
+
+	/**
+	 * Set the application object
+	 *
+	 * @param \Illuminate\Container\Container $application
+	 */
+	public function setApplication(Container $application)
+	{
+		$this->app = $application;
+	}
+
+	/**
+	 * Get the application object
+	 *
+	 * @return \Illuminate\Container\Container
+	 */
+	public function getApplication()
+	{
+		return $this->app;
 	}
 
 	/**
@@ -17,5 +42,15 @@ abstract class Provider {
 	 * @return void
 	 */
 	abstract public function register();
+
+	/**
+	 * A list of bindings that this provides to the application instance
+	 *
+	 * @return string[]
+	 */
+	public function provides()
+	{
+		return [];
+	}
 
 }
---
+++ web/concrete/src/Http/Middleware/Concrete/SessionMiddleware.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Concrete\Core\Http\Middleware\Concrete;
+
+class SessionMiddleware
+{
+
+}
